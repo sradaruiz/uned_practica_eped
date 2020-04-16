f5a6ed907ebf33300c6ae3f7c083d305d519a6cb
dariopascu <dariopascu@gmail.com>
2020-04-17 00:06:30 +0200

    WordList Tree first version

--- src/es/uned/lsi/eped/pract2019_2020/WordListN.java
+++ src/es/uned/lsi/eped/pract2019_2020/WordListN.java
@@ -1,42 +1,65 @@
 package es.uned.lsi.eped.pract2019_2020;
 
+import es.uned.lsi.eped.DataStructures.BTree;
+import es.uned.lsi.eped.DataStructures.BTreeIF;
+
 public class WordListN {
-	/* Atributos de la clase con la estructura adecuada */
-	...
-	/* Atributos de la clase con la estructura adecuada */
+    /* Atributos de la clase con la estructura adecuada */
+    int wordSize;
+    BTreeIF<String> words;
 
-	public WordListN(int size) {
+    /* Atributos de la clase con la estructura adecuada */
+    public WordListN(int size) {
+        wordSize = size;
+    }
 
-	}
-	
-	public void add(String word) {
+    public void add(String word) {
+        words = addChild(words, word);
+    }
 
-	}
-	
-	public int getWordSize() {
+    private BTreeIF<String> addChild(BTreeIF<String> child, String word) {
+        if (child == null) {
+            BTreeIF<String> newChild = new BTree<String>();
+            newChild.setRoot(word);
+            return newChild;
+        }
+        int wordCompare = word.compareToIgnoreCase(child.getRoot());
+        if (wordCompare < 0) {
+            child.setLeftChild(addChild(child.getLeftChild(), word));
+        } else if (wordCompare > 0) {
+            child.setRightChild(addChild(child.getRightChild(), word));
+        } else {
+            return child;
+        }
+        return child;
+    }
 
-	}
-	
-	public String toString() {
-		StringBuilder salida = new StringBuilder();
-		int numPalabras = /* Longitud de la secuencia de palabras */
-		salida.append("-Palabras de ");
-		salida.append(this.getWordSize());
-		salida.append(" letra");
-		if ( this.getWordSize() > 1 ) { salida.append('s'); }
-		salida.append(": ");
-		for (int pos = 1 ; pos <= numPalabras ; pos++) {
-			/* Estas líneas dependen de la estructura escogida */
-			String word = /* Obtener la siguiente palabra */
-			/* Avanzar a la siguiente sin destruir la estructura */
+    public int getWordSize() {
+        return wordSize;
+    }
+
+    public String toString() {
+        StringBuilder salida = new StringBuilder();
+        int numPalabras = words.size(); /*Longitud de la secuencia de palabras */
+        salida.append("-Palabras de ");
+        salida.append(this.getWordSize());
+        salida.append(" letra");
+        if (this.getWordSize() > 1) {
+            salida.append('s');
+        }
+        salida.append(": ");
+        for (int pos = 1; pos <= numPalabras; pos++) {
+            /* Estas líneas dependen de la estructura escogida */
+            String word = words/* Obtener la siguiente palabra */
+            /* Avanzar a la siguiente sin destruir la estructura */
 			...
-			/* Estas líneas dependen de la estructura escogida */
-			salida.append(word);
-			if ( pos < numPalabras ) {
-				salida.append(", ");
-			}
-		}
-		salida.append('\n');
-		return salida.toString();
-	}
+            /* Estas líneas dependen de la estructura escogida */
+            salida.append(word);
+            if (pos < numPalabras) {
+                salida.append(", ");
+            }
+        }
+        salida.append('\n');
+        return salida.toString();
+    }
 }
---
+++ src/es/uned/lsi/eped/pract2019_2020/WordListTest.java
@@ -0,0 +1,14 @@
+package es.uned.lsi.eped.pract2019_2020;
+
+public class WordListTest {
+    public static void main(String[] args) {
+        WordListN list = new WordListN(1);
+        list.add("m");
+        list.add("c");
+        list.add("a");
+        list.add("z");
+        list.add("t");
+        list.add("r");
+
+    }
+}
