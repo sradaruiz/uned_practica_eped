fd27eafea4bb3102d78b83066f4bff9c3735210c
dariopascu <dariopascu@gmail.com>
2020-04-17 21:15:43 +0200

    better order

--- src/es/uned/lsi/eped/pract2019_2020/WordListN.java
+++ src/es/uned/lsi/eped/pract2019_2020/WordListN.java
@@ -25,9 +25,15 @@ public class WordListN {
         }
         int wordCompare = word.compareToIgnoreCase(child.getRoot());
         if (wordCompare < 0) {
-            child.setLeftChild(addChild(child.getLeftChild(), word));
+            String oldRoot = child.getRoot();
+            child.setRoot(word);
+            child.setLeftChild(addChild(child.getLeftChild(), oldRoot));
         } else if (wordCompare > 0) {
-            child.setRightChild(addChild(child.getRightChild(), word));
+            if (word.compareToIgnoreCase(child.getLeftChild().getRoot()) < 0) {
+                child.setLeftChild(addChild(child.getLeftChild(), word));
+            } else {
+                child.setRightChild(addChild(child.getRightChild(), word));
+            }
         } else {
             return child;
         }
@@ -38,28 +44,28 @@ public class WordListN {
         return wordSize;
     }
 
-    public String toString() {
-        StringBuilder salida = new StringBuilder();
-        int numPalabras = words.size(); /*Longitud de la secuencia de palabras */
-        salida.append("-Palabras de ");
-        salida.append(this.getWordSize());
-        salida.append(" letra");
-        if (this.getWordSize() > 1) {
-            salida.append('s');
-        }
-        salida.append(": ");
-        for (int pos = 1; pos <= numPalabras; pos++) {
-            /* Estas líneas dependen de la estructura escogida */
-            String word = words/* Obtener la siguiente palabra */
-            /* Avanzar a la siguiente sin destruir la estructura */
-			...
-            /* Estas líneas dependen de la estructura escogida */
-            salida.append(word);
-            if (pos < numPalabras) {
-                salida.append(", ");
-            }
-        }
-        salida.append('\n');
-        return salida.toString();
-    }
+//    public String toString() {
+//        StringBuilder salida = new StringBuilder();
+//        int numPalabras = words.size(); /*Longitud de la secuencia de palabras */
+//        salida.append("-Palabras de ");
+//        salida.append(this.getWordSize());
+//        salida.append(" letra");
+//        if (this.getWordSize() > 1) {
+//            salida.append('s');
+//        }
+//        salida.append(": ");
+//        for (int pos = 1; pos <= numPalabras; pos++) {
+//            /* Estas líneas dependen de la estructura escogida */
+//            String word = words/* Obtener la siguiente palabra */
+//            /* Avanzar a la siguiente sin destruir la estructura */
+//			...
+//            /* Estas líneas dependen de la estructura escogida */
+//            salida.append(word);
+//            if (pos < numPalabras) {
+//                salida.append(", ");
+//            }
+//        }
+//        salida.append('\n');
+//        return salida.toString();
+//    }
 }
