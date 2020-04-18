f502f1cf77fdb93b9c93d721910906c46d72b6b0
dariopascu <dariopascu@gmail.com>
2020-04-18 13:37:44 +0200

    WordListN completed

--- src/es/uned/lsi/eped/pract2019_2020/WordListN.java
+++ src/es/uned/lsi/eped/pract2019_2020/WordListN.java
@@ -7,83 +7,71 @@ public class WordListN {
     /* Atributos de la clase con la estructura adecuada */
     int wordSize;
     BTreeIF<String> words;
-
     /* Atributos de la clase con la estructura adecuada */
+
     public WordListN(int size) {
         wordSize = size;
-    }
-
-    public void add(String word) {
-        words = addChild(words, word);
-    }
-
-    private BTreeIF<String> addChild(BTreeIF<String> child, String word) {
-        // TODO: controlar la insercion de duplicados
-        if (child == null) {
-            BTreeIF<String> newChild = new BTree<String>();
-            newChild.setRoot(word);
-            return newChild;
-        }
-        int wordCompare = word.compareToIgnoreCase(child.getRoot());
-        if (wordCompare < 0) {
-            String oldRoot = child.getRoot();
-            child.setRoot(word);
-            if (child.getLeftChild() == null) {
-                child.setLeftChild(addChild(child.getLeftChild(), oldRoot));
-            } else if (oldRoot.compareToIgnoreCase(child.getLeftChild().getRoot()) < 0) {
-                String leftChildRoot = child.getLeftChild().getRoot();
-                child.setLeftChild(addChild(child.getLeftChild(), oldRoot));
-                child.setRightChild(addChild(child.getRightChild(), leftChildRoot));
-            } else if (oldRoot.compareToIgnoreCase(child.getLeftChild().getRoot()) > 0) {
-                child.setRightChild(addChild(child.getRightChild(), oldRoot));
-            } else {
-                return child;
-            }
-//            child.setLeftChild(addChild(child.getLeftChild(), oldRoot));
-        } else if (wordCompare > 0) {
-            if (child.getLeftChild() == null) {
-                child.setLeftChild(addChild(child.getLeftChild(), word));
-            } else if (word.compareToIgnoreCase(child.getLeftChild().getRoot()) < 0) {
-                String leftChildRoot = child.getLeftChild().getRoot();
-                child.setLeftChild(addChild(child.getLeftChild(), word));
-                child.setRightChild(addChild(child.getRightChild(), leftChildRoot));
-            } else if (word.compareToIgnoreCase(child.getLeftChild().getRoot()) > 0) {
-                child.setRightChild(addChild(child.getRightChild(), word));
-            } else {
-                return child;
-            }
-        } else {
-            return child;
-        }
-        return child;
     }
 
     public int getWordSize() {
         return wordSize;
     }
 
-//    public String toString() {
-//        StringBuilder salida = new StringBuilder();
-//        int numPalabras = words.size(); /*Longitud de la secuencia de palabras */
-//        salida.append("-Palabras de ");
-//        salida.append(this.getWordSize());
-//        salida.append(" letra");
-//        if (this.getWordSize() > 1) {
-//            salida.append('s');
-//        }
-//        salida.append(": ");
-//        for (int pos = 1; pos <= numPalabras; pos++) {
-//            /* Estas líneas dependen de la estructura escogida */
-//            String word = words/* Obtener la siguiente palabra */
-//            /* Avanzar a la siguiente sin destruir la estructura */
-//			...
-//            /* Estas líneas dependen de la estructura escogida */
-//            salida.append(word);
-//            if (pos < numPalabras) {
-//                salida.append(", ");
-//            }
-//        }
-//        salida.append('\n');
-//        return salida.toString();
-//    }
+    public void add(String word) {
+        words = addChild(words, word);
+    }
+
+    private BTreeIF<String> addChild(BTreeIF<String> tree, String word) {
+        if (word.length() != wordSize) {
+            // No se insertan palabras que no cuadran con el tamaño indicado
+            return tree;
+        }
+        if (tree == null) {
+            // Si el árbol no está iniciado se inicia
+            BTreeIF<String> newChild = new BTree<String>();
+            newChild.setRoot(word);
+            return newChild;
+        }
+        int wordCompare = word.compareToIgnoreCase(tree.getRoot());
+        if (wordCompare < 0) {
+            tree.setLeftChild(addChild(tree.getLeftChild(), word));
+        } else if (wordCompare > 0) {
+            tree.setRightChild(addChild(tree.getRightChild(), word));
+        } else {
+            return tree;
+        }
+        return tree;
+    }
+
+    public String toString() {
+        StringBuilder salida = new StringBuilder();
+        int numPalabras = words.size(); /*Longitud de la secuencia de palabras */
+        salida.append("-Palabras de ");
+        salida.append(this.getWordSize());
+        salida.append(" letra");
+        if (this.getWordSize() > 1) {
+            salida.append('s');
+        }
+        salida.append(": ");
+        salida.append(getWords());
+        salida.append('\n');
+        return salida.toString();
+    }
+
+    private StringBuilder getWordsFromTree(BTreeIF<String> tree, StringBuilder salida) {
+        if (tree == null) {
+            return new StringBuilder();
+        }
+
+        getWordsFromTree(tree.getLeftChild(), salida);
+        salida.append(", ").append(tree.getRoot());
+        getWordsFromTree(tree.getRightChild(), salida);
+
+        return salida;
+    }
+
+    private String getWords() {
+        StringBuilder wordsList = getWordsFromTree(words, new StringBuilder());
+        return wordsList.substring(wordsList.indexOf(",") + 1, wordsList.length());
+    }
 }
--- src/es/uned/lsi/eped/pract2019_2020/WordListTest.java
+++ src/es/uned/lsi/eped/pract2019_2020/WordListTest.java
@@ -2,13 +2,16 @@ package es.uned.lsi.eped.pract2019_2020;
 
 public class WordListTest {
     public static void main(String[] args) {
-        WordListN list = new WordListN(1);
-        list.add("e");
-        list.add("c");
-        list.add("a");
-        list.add("b");
-        list.add("d");
-        list.add("r");
+        WordListN list = new WordListN(4);
+        list.add("baca");
+        list.add("casa");
+        list.add("alta");
+        list.add("zapa");
+        list.add("cama");
+        list.add("dami");
+        list.add("alta");
+        list.add("al");
 
+        System.out.println(list.toString());
     }
 }
